aac393fa1fec87cd28484a15939ff61bd031b91c
Max Sabate <max.sabate@gmail.com>
2022-06-28 16:29:00 -0700

    pages and routes set up

--- src/App.js
+++ src/App.js
@@ -1,9 +1,39 @@
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+ 
+// syles
 import './App.css'
+
+// pages and components
+import Dashboard from './pages/dashboard/Dashboard'
+import Create from './pages/create/Create'
+import Login from './pages/login/Login'
+import Signup from './pages/signup/Signup'
+import Project from './pages/project/Project'
 
 function App() {
   return (
     <div className="App">
-
+        <BrowserRouter>
+          <div className="container">
+            <Switch>
+              <Route exact path='/'>
+                <Dashboard />
+              </Route>
+              <Route exact path='/create'>
+                <Create />
+              </Route>
+              <Route exact path='/login'>
+                <Login />
+              </Route>
+              <Route exact path='/projects/:id'>
+                <Project />
+              </Route>
+              <Route exact path='/signup'>
+                <Signup />
+              </Route>
+            </Switch>
+          </div>
+        </BrowserRouter>
     </div>
   );
 }
